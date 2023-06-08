089f353597eba4aa7f662f725151036c2f5ab39a
Szymon Zackiewicz <zacszym@gmail.com>
2023-06-08 19:41:12 +0200

    jetty jsp (not working)

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -7,7 +7,28 @@ import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.webapp.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
+import org.eclipse.jetty.jsp.JettyJspServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+
+/**
+ * Starts up the server, including a DefaultServlet that handles static files,
+ * and any servlet classes annotated with the @WebServlet annotation.
+ */
 public class App {
 
     private static FridgeRepository fridgeRepository = new FridgeRepository();
@@ -16,37 +37,135 @@ public class App {
 
         fridgeRepository.deleteUnusedFridge();
 
-        var webapp = new WebAppContext();
-        webapp.setResourceBase("src/main/webapp");
-        webapp.setContextPath("/");
-        webapp.setInitParameter("org.eclipse.jetty.servlet.Default.maxCachedFiles", "0");
-        webapp.setConfigurations(new Configuration[]
-                {
-                        new AnnotationConfiguration(),
-                        new WebInfConfiguration(),
-                        new WebXmlConfiguration(),
-                        new MetaInfConfiguration(),
-                        new FragmentConfiguration(),
-                        new EnvConfiguration(),
-                        new PlusConfiguration(),
-                        new JettyWebXmlConfiguration()
-                });
-        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+        // Create a server that listens on port 8080.
+        Server server = new Server(8080);
+        WebAppContext webAppContext = new WebAppContext();
+        server.setHandler(webAppContext);
 
-//        webapp.addServlet(HelloServlet.class,"/api/*");
-        var server = new Server(8080);
-        server.setHandler(webapp);
+        // Load static content from inside the jar file.
+        webAppContext.setResourceBase("src/main/webapp");
+        webAppContext.setContextPath("/");
+        webAppContext.setInitParameter("org.eclipse.jetty.servlet.Default.maxCachedFiles", "0");
+
+        // Enable annotations so the server sees classes annotated with @WebServlet.
+        webAppContext.setConfigurations(new Configuration[]{
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new MetaInfConfiguration(),
+                new FragmentConfiguration(),
+                new EnvConfiguration(),
+                new PlusConfiguration(),
+                new JettyWebXmlConfiguration()
+        });
+
+        // Look for annotations in the classes directory (dev server) and in the
+        // jar file (live server)
+        webAppContext.setAttribute(
+                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/target/classes/|.*\\.jar");
+
+        // Handle static resources, e.g. html files.
+        webAppContext.addServlet(DefaultServlet.class, "/");
+
+        // Configure JSP support.
+        enableEmbeddedJspSupport(webAppContext);
+
         server.addLifeCycleListener(new LifeCycle.Listener() {
             @Override
             public void lifeCycleStopped(LifeCycle event) {
                 HibernateUtil.close();
             }
         });
-
+        // Start the server! 🚀
         server.start();
+        System.out.println("Server started!");
 
+        // Keep the main thread alive while the server is running.
         server.join();
-
-
     }
-}
+
+    /**
+     * Setup JSP Support for ServletContextHandlers.
+     * <p>
+     *   NOTE: This is not required or appropriate if using a WebAppContext.
+     * </p>
+     *
+     * @param servletContextHandler the ServletContextHandler to configure
+     * @throws IOException if unable to configure
+     */
+    private static void enableEmbeddedJspSupport(ServletContextHandler servletContextHandler) throws IOException
+    {
+        // Establish Scratch directory for the servlet context (used by JSP compilation)
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
+
+        if (!scratchDir.exists())
+        {
+            if (!scratchDir.mkdirs())
+            {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+        servletContextHandler.setAttribute("javax.servlet.context.tempdir", scratchDir);
+        //servletContextHandler.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+
+
+
+        // Set Classloader of Context to be sane (needed for JSTL)
+        // JSP requires a non-System classloader, this simply wraps the
+        // embedded System classloader in a way that makes it suitable
+        // for JSP to use
+        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], App.class.getClassLoader());
+        servletContextHandler.setClassLoader(jspClassLoader);
+
+        // Manually call JettyJasperInitializer on context startup
+
+
+        // Create / Register JSP Servlet (must be named "jsp" per spec)
+        ServletHolder holderJsp = new ServletHolder("jsp", JettyJspServlet.class);
+        holderJsp.setInitOrder(0);
+        holderJsp.setInitParameter("logVerbosityLevel", "DEBUG");
+        holderJsp.setInitParameter("fork", "false");
+        holderJsp.setInitParameter("xpoweredBy", "false");
+        holderJsp.setInitParameter("compilerTargetVM", "1.8");
+        holderJsp.setInitParameter("compilerSourceVM", "1.8");
+        holderJsp.setInitParameter("keepgenerated", "true");
+        servletContextHandler.addServlet(holderJsp, "*.jsp");
+    }
+
+
+            }
+
+
+
+//public class App {
+//
+//
+//
+//    public static void main(String[] args) throws Exception {
+//
+//
+//
+//        var webapp = new WebAppContext();
+//        webapp.setResourceBase("src/main/webapp");
+//        webapp.setContextPath("/");
+//        webapp.setInitParameter("org.eclipse.jetty.servlet.Default.maxCachedFiles", "0");
+//        webapp.setConfigurations(new Configuration[]
+//                {
+//
+//                });
+//
+//
+////        webapp.addServlet(HelloServlet.class,"/api/*");
+//        var server = new Server(8080);
+//        server.setHandler(webapp);
+//
+//
+//        server.start();
+//
+//        server.join();
+//
+//
+//    }
+//}
