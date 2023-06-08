9361c81eebddb6acba1c84c544035a33212b6f53
Szymon Zackiewicz <zacszym@gmail.com>
2023-06-08 17:31:54 +0200

    working locally

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -1,37 +1,22 @@
 package io.github.mat3e;
 
 import io.github.mat3e.fridge.FridgeRepository;
-import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
-import org.eclipse.jetty.jsp.JettyJspServlet;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.component.AbstractLifeCycle;
 import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.webapp.*;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 public class App {
 
     private static FridgeRepository fridgeRepository = new FridgeRepository();
 
     public static void main(String[] args) throws Exception {
-        var server = new Server(8080);
 
-   fridgeRepository.deleteUnusedFridge();
+        fridgeRepository.deleteUnusedFridge();
 
         var webapp = new WebAppContext();
-
-        server.setHandler(webapp);
-
         webapp.setResourceBase("src/main/webapp");
         webapp.setContextPath("/");
         webapp.setInitParameter("org.eclipse.jetty.servlet.Default.maxCachedFiles", "0");
@@ -49,104 +34,19 @@ public class App {
         webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
 
 //        webapp.addServlet(HelloServlet.class,"/api/*");
-
-
-        server.addEventListener(new LifeCycle.Listener() {
+        var server = new Server(8080);
+        server.setHandler(webapp);
+        server.addLifeCycleListener(new LifeCycle.Listener() {
             @Override
             public void lifeCycleStopped(LifeCycle event) {
                 HibernateUtil.close();
             }
         });
 
-        // Configure JSP support.
-        enableEmbeddedJspSupport(webapp);
-
         server.start();
 
         server.join();
 
 
-    }
-    /**
-     * Setup JSP Support for ServletContextHandlers.
-     * <p>
-     *   NOTE: This is not required or appropriate if using a WebAppContext.
-     * </p>
-     *
-     * @param servletContextHandler the ServletContextHandler to configure
-     * @throws IOException if unable to configure
-     */
-    private static void enableEmbeddedJspSupport(ServletContextHandler servletContextHandler) throws IOException
-    {
-        // Establish Scratch directory for the servlet context (used by JSP compilation)
-        File tempDir = new File(System.getProperty("java.io.tmpdir"));
-        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
-
-        if (!scratchDir.exists())
-        {
-            if (!scratchDir.mkdirs())
-            {
-                throw new IOException("Unable to create scratch directory: " + scratchDir);
-            }
-        }
-        servletContextHandler.setAttribute("javax.servlet.context.tempdir", scratchDir);
-
-        // Set Classloader of Context to be sane (needed for JSTL)
-        // JSP requires a non-System classloader, this simply wraps the
-        // embedded System classloader in a way that makes it suitable
-        // for JSP to use
-        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], App.class.getClassLoader());
-        servletContextHandler.setClassLoader(jspClassLoader);
-
-        // Manually call JettyJasperInitializer on context startup
-        servletContextHandler.addBean(new JspStarter(servletContextHandler));
-
-        // Create / Register JSP Servlet (must be named "jsp" per spec)
-        ServletHolder holderJsp = new ServletHolder("jsp", JettyJspServlet.class);
-        holderJsp.setInitOrder(0);
-        holderJsp.setInitParameter("logVerbosityLevel", "DEBUG");
-        holderJsp.setInitParameter("fork", "false");
-        holderJsp.setInitParameter("xpoweredBy", "false");
-        holderJsp.setInitParameter("compilerTargetVM", "1.8");
-        holderJsp.setInitParameter("compilerSourceVM", "1.8");
-        holderJsp.setInitParameter("keepgenerated", "true");
-        servletContextHandler.addServlet(holderJsp, "*.jsp");
-    }
-
-    /**
-     * JspStarter for embedded ServletContextHandlers
-     *
-     * This is added as a bean that is a jetty LifeCycle on the ServletContextHandler.
-     * This bean's doStart method will be called as the ServletContextHandler starts,
-     * and will call the ServletContainerInitializer for the jsp engine.
-     *
-     */
-    public static class JspStarter extends AbstractLifeCycle implements ServletContextHandler.ServletContainerInitializerCaller
-    {
-        JettyJasperInitializer sci;
-        ServletContextHandler context;
-
-        public JspStarter (ServletContextHandler context)
-        {
-            this.sci = new JettyJasperInitializer();
-            this.context = context;
-            this.context.setAttribute("org.apache.tomcat.JarScanner", new StandardJarScanner());
-        }
-
-        @Override
-        protected void doStart() throws Exception
-        {
-            ClassLoader old = Thread.currentThread().getContextClassLoader();
-            Thread.currentThread().setContextClassLoader(context.getClassLoader());
-            try
-            {
-                sci.onStartup(null, context.getServletContext());
-                super.doStart();
-            }
-            finally
-            {
-                Thread.currentThread().setContextClassLoader(old);
-            }
-        }
     }
 }
